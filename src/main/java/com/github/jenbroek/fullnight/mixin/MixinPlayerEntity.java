b66b8a47b2bb5ab2595d2398fd57b6fc447879b8
Jens Broekens <jens.broekens@protonmail.com>
2024-11-19 00:06:32 +0100

    prevent weaker effect from overwriting if enabled

---
+++ src/main/java/com/github/jenbroek/fullnight/mixin/MixinPlayerEntity.java
@@ -0,0 +1,29 @@
+package com.github.jenbroek.fullnight.mixin;
+
+import com.github.jenbroek.fullnight.ClientInitializer;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(PlayerEntity.class)
+abstract class MixinPlayerEntity extends LivingEntity {
+
+	protected MixinPlayerEntity(EntityType<? extends LivingEntity> entityType, World world) {
+		super(entityType, world);
+	}
+
+	@Override
+	public boolean addStatusEffect(StatusEffectInstance statusEffectInstance, @Nullable Entity entity) {
+		if (ClientInitializer.ON && StatusEffects.NIGHT_VISION.matches(statusEffectInstance.getEffectType()) && this.hasStatusEffect(StatusEffects.NIGHT_VISION)) {
+			return true;
+		}
+		return super.addStatusEffect(statusEffectInstance, entity);
+	}
+
+}
